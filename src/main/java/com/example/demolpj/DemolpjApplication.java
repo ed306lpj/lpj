a69565f422ec0dceb435f0dfb0e2dfa085472458
刘鹏举 <568331921@qq.com>
2021-12-19 23:46:26 +0800

    werew

---
+++ src/main/java/com/example/demolpj/DemolpjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demolpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolpjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemolpjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demolpj/DemolpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demolpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolpjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
